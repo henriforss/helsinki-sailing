386bf9b224eeb65f86bd71fa8f9acb3d969a087a
Henri Forss <hforss@gmail.com>
2022-05-27 13:22:03 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = process.env.REACT_APP_API_KEY
+
+const LikeButton = () => {
+  const [likeButton, setLikeButton] = useState(false)
+
+  const handleLikeButton = () => {
+    setLikeButton(!likeButton)
+    console.log(likeButton)
+  }
+
+  if (likeButton === false) {
+    return (
+      <div>
+        <button onClick={handleLikeButton}>Press here to like</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        You like this!
+      </div>
+    )
+  }
+
 }
 
-export default App;
+const ShowWeather = () => {
+  const [weather, setWeather] = useState(null)
+
+  const weatherUrl = new URL('https://api.openweathermap.org/data/2.5/weather')
+
+  weatherUrl.searchParams.set('q', 'Helsinki')
+  weatherUrl.searchParams.set('appid', `${apiKey}`)
+  weatherUrl.searchParams.set('units', 'metric')
+
+  const getWeather = () => {
+    axios
+      .get(weatherUrl)
+      .then((response) => {
+        setWeather(response.data)
+      })
+    }
+    
+  useEffect(getWeather, [])
+  console.log(weather)
+
+  if (weather == null) {
+    return (
+      <div>
+        No data
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <ul>
+          <li>Temperature: {weather.main.temp} Celsius</li>
+          <li>Feels like: {weather.main.feels_like} Celsius</li>
+          <li>Humidity: {weather.main.humidity} %</li>
+          <li>Pressure: {weather.main.pressure} hPa</li>
+          <li>Clouds: {weather.clouds.all} %</li>
+          <li>Rain: {weather.rain['1h']} mm/1h</li>
+          <li>Wind: {weather.wind.speed} m/s</li>
+          <li>Wind direction: {weather.wind.deg} degrees</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  return (
+    <div>
+      <h2>Helsinki Sailing Weather</h2>
+      <ShowWeather />
+      <LikeButton />
+    </div>
+  )
+}
+
+export default App
